73fe49b7bfecb6e5514bdf57d6546dfe2848cd62
jinc132 <jinc132@u.washington.edu>
2019-08-15 17:21:09 -0700

    set up navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
 import About from './pages/About';
 import Home from './pages/Home';
+import resume from './pages/jinresume.pdf';
+import logo from './css/logo.png';
 import './css/App.css';
 
+const menuItems = [
+  'Home',
+  'About',
+  'Projects'
+];
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      isOpen: false
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+  
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
   render() {
     const App = () => (
       <div>
@@ -14,10 +40,29 @@ class App extends Component {
         </Switch>
       </div>
     )
+
     return (
-      <Switch>
-        <App/>
-      </Switch>
+      <div>
+        <Navbar color="dark" light expand="md">
+          <NavbarBrand href="/"><img src={logo} alt="portfolio logo" style={{ width: '3em'}}/></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              {menuItems.map(menuItem => 
+                <NavItem> 
+                  <NavLink href={'/' + menuItem}>{menuItem}</NavLink>
+                </NavItem>
+              )}
+              <NavItem>
+                <NavLink href={resume} target="_blank" >Resume</NavLink>
+                </NavItem>
+              </Nav>
+          </Collapse>
+        </Navbar>  
+        <Switch>
+          <App/>
+        </Switch>
+      </div>
     );
   }
 }
