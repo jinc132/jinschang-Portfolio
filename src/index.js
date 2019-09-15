df577d18cc2cd003cda6e165162d06eb7da2e740
jinc132 <jinc132@u.washington.edu>
2019-09-14 19:59:28 -0700

    change icon designs

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import './css/index.css';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
