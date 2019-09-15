6eec6f96d5749643c1fbc8c0ec9ca4db195025ee
jinc132 <jinc132@u.washington.edu>
2019-09-15 00:18:37 -0700

    Show current page tab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import { NavLink, NavItem } from 'reactstrap';
 import About from './pages/About';
 import Home from './pages/Home';
@@ -7,46 +7,37 @@ import resume from './pages/jinresume.pdf';
 import './css/App.css';
 
 class App extends Component {
-  handleSelected(url) {
-    // reset all the menu items
-    let elems = document.querySelectorAll("#navbar .nav-item .nav-link");
-    for (let index = 0; index < elems.length; index++) {
-      if (elems[index].innerHTML === url) {
-        elems[index].classList.add('active');
-      }
-    }
-  }
-
   render() {
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Projects' component={Home}/>
           <Route path='/About' component={About} />
         </Switch>
       </div>
     )
-
+    
     let menu = (
-      <div className="navi">
-        <ul id="navbar">
-          <div className="work">
-            <NavItem onClick={this.handleSelected("Projects")}>
-              <NavLink href="#projects">Projects</NavLink>
+      <div className='navi'>
+        <ul id='navbar'>
+          <div className='work'>
+            <NavItem className={this.props.history.location.pathname === '/Projects' ? 'active' : ''}>
+              <NavLink href='/Projects'>Projects</NavLink>
             </NavItem>
-            <NavItem onClick={this.handleSelected("Resume")}>
-              <NavLink href={resume} target="_blank" >Resume</NavLink>
+            <NavItem>
+              <NavLink href={resume} target='_blank'>Resume</NavLink>
             </NavItem>
           </div>
-          <div className="nav-brand">
-            <NavLink className="brand" href="/">Jin Chang</NavLink>
+          <div className='nav-brand'>
+            <NavLink className='brand' href='/'>Jin Chang</NavLink>
           </div>
-          <div className="personal">
-            <NavItem onClick={this.handleSelected("About")}>
-              <NavLink href="/About">About</NavLink>
+          <div className='personal'>
+            <NavItem className={this.props.history.location.pathname === '/About' ? 'active' : ''}>
+              <NavLink href='/About'>About</NavLink>
             </NavItem>
-            <NavItem onClick={this.handleSelected("Contact")}>
-              <NavLink href="mailto:Jinc132@gmail.com">Contact</NavLink>
+            <NavItem>
+              <NavLink href='mailto:Jinc132@gmail.com'>Contact</NavLink>
             </NavItem>
           </div>
         </ul>
@@ -63,4 +54,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
