c9191fb238f0e9dc4942c8a1ad54c89cd30b1d29
jinc132 <jinc132@u.washington.edu>
2019-09-15 02:37:29 -0700

    clean up and add required content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import { Link } from 'react-router-dom';
 import '../css/about.css';
+import '../css/home.css';
 import picture from '../css/img/jinPicture.png';
 class About extends Component {
     render() {
@@ -15,87 +16,34 @@ class About extends Component {
                 <div className="jinBio">
                     <h1>Who is Jin?</h1>
                     <p>
-                        My name is Jin Chang, a recent graduate from <a href="https://artsci.washington.edu/" rel="noopener noreferrer" target="_blank">University of Washington College of Arts and Sciences</a>.
-                        I received my bachelor's degree in <a href="https://geography.washington.edu/" rel="noopener noreferrer" target="_blank">Geography</a> and minored in <a href="https://ischool.uw.edu/programs/informatics" rel="noopener noreferrer" target="_blank">Informatics</a>.
-                        During this period, I frequently spent my nights on coding, designing, debugging, drinking coffee, and with out much sleep. However, although it was a long and arduous journey I still survived and in turn gained invaluable skills. 
-                        These experiences and obstacles each had a significant impact in shaping me into the person that I am today. 
+                        I am recent graduate from <a href="https://artsci.washington.edu/" rel="noopener noreferrer" target="_blank">University of Washington College of Arts and Sciences</a>.
+                        I received my bachelor's degree in <a href="https://geography.washington.edu/" rel="noopener noreferrer" target="_blank">Geographic Information Systems</a> and minored in <a href="https://ischool.uw.edu/programs/informatics" rel="noopener noreferrer" target="_blank">Informatics</a>.
+                            
                     </p>
                 </div>
-                <div className="bioList">
-                    <div className="list">
-                        <h1>You can usually find me: </h1>
-                        <ul>
-                            <li>
-                                Colaborating on an interesting project
-                            </li>
-                            <li>
-                                Reading coding blogs
-                            </li>
-                            <li>
-                                Sketching out designs or pseudocode on the whiteboard
-                            </li>
-                            <li>
-                                Creating random sketches on Figma
-                            </li>
-                        </ul>
-                        <h1>How about Passions?</h1>
-                        <p>
-                            As for my current passions, I am interested in learning about developing microservices and RESTful APIs. Even though I 
-                            have multiple different skills under my belt, I am not a expert. I am familiar and confident in my skills but, definitely not a expert since I 
-                            still have so much to learn and explore. 
-                        </p>
-                    </div>
-                    <div className="portrait">
+                <div className='email-wrapper'>
+                    <div className="email-container">
+                        <a href="mailto:Jinc132@gmail.com">
+                            <h4>Let's Talk</h4>
+                        </a>
                     </div>
                 </div>
-                <div className="contact">
-                    <div className="skew"></div>
-                    <div className="contactContainer">
-                        <div className="message">
-                            <div className="descContainer">
-                                <h1>Contact Me</h1>
-                                <h2>Got Questions? Drop me a line</h2>
-                            </div>
-                            <Link to={"/Contact"}>
-                                <button>
-                                    <i className="far fa-paper-plane"></i>
-                                    <h2>Message Me</h2>
-                                </button>
-                            </Link>
+                <div className="social-area">
+                    <a href="https://github.com/jinc132" rel="noopener noreferrer" target="_blank">
+                        <div className="circle">
+                            <i className="fa fa-github" id="social-media"></i>
                         </div>
-                        <div className="otherContact">
-                            <div className="emailContainer">
-                                <div className="circle">
-                                    <i className="far fa-envelope"></i>
-                                </div>
-                                <div className="email">
-                                    <a href="mailto:Jinc132@gmail.com">
-                                        <h2>Jinc132@gmail.com</h2>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="linkedinContainer">
-                                <div className="circle">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </div>
-                                <div className="linkedin">
-                                    <a href="https://www.linkedin.com/in/jin-s-chang/">
-                                        <h2>Via Linkedin</h2>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="instagramContainer">
-                                <div className="circle">
-                                    <i className="fab fa-instagram"></i>
-                                </div>
-                                <div className="instagram">
-                                    <a href="https://www.instagram.com/jc_muzer/">
-                                        <h2>Via Instagram</h2>
-                                    </a>
-                                </div>
-                            </div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/jinschang" rel="noopener noreferrer" target="_blank">
+                        <div className="circle">
+                            <i className="fa fa-linkedin" id="social-media"></i>
                         </div>
-                    </div>
+                    </a>
+                    <a href="https://www.instagram.com/jc_muzer/?hl=en" rel="noopener noreferrer" target="_blank">
+                        <div className="circle">
+                            <i className="fa fa-instagram" id="social-media"></i>
+                        </div>
+                    </a>
                 </div>
             </div>
         );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,14 @@ class Home extends Component {
         </div>
         <div className="projects" id="projects">
           <div className="double-item-row">
-            <a className="item-link" href="https://github.com/info442-sp19-undefined/hw7" target="_blank" style={{backgroundColor:'#87C0CD'}}>
+            <a className="item-link" href="https://github.com/info442-sp19-undefined/hw7" rel="noopener noreferrer" target="_blank" style={{backgroundColor:'#87C0CD'}}>
               <div id="icebreaker" className="proj-image">
                 <div className="hover-overlay">
                   <h2>Dive In!</h2>
                 </div>
               </div>
             </a>
-            <a className="item-link" href="https://poverty469.github.io/span-mapp/#/tour" target="_blank" style={{backgroundColor:'#fff6f2'}}>
+            <a className="item-link" href="https://poverty469.github.io/span-mapp/#/tour" rel="noopener noreferrer" target="_blank" style={{backgroundColor:'#fff6f2'}}>
               <div id="span" className="proj-image">
                 <div className="hover-overlay">
                   <h2>Statewide Poverty Action Network</h2>
@@ -25,7 +25,7 @@ class Home extends Component {
             </a>
           </div>
           <div className="single-item-row">
-            <a className="item-link" href="https://github.com/jinc132/CeliacRecipes" target="_blank" style={{backgroundColor:'wheat'}}>
+            <a className="item-link" href="https://github.com/jinc132/CeliacRecipes" rel="noopener noreferrer" target="_blank" style={{backgroundColor:'wheat'}}>
               <div id="celiac" className="proj-image">
                 <div className="hover-overlay">
                   <h2>Celiac Recipes</h2>
@@ -34,14 +34,14 @@ class Home extends Component {
             </a>
           </div>
           <div className="double-item-row">
-            <a className="item-link" href="https://github.com/jinc132/MovieApp" target="_blank" style={{backgroundColor:'rgb(226, 87, 87)'}}>
+            <a className="item-link" href="https://github.com/jinc132/MovieApp" rel="noopener noreferrer" target="_blank" style={{backgroundColor:'rgb(226, 87, 87)'}}>
               <div id="movieApp" className="proj-image">
                 <div className="hover-overlay">
                   <h2>Movie Rate</h2>
                 </div>
               </div>
             </a>
-            <a className="item-link" href="https://github.com/carmelitadeleon/GEOG458_Final" target="_blank" style={{backgroundColor:'rgb(139, 139, 137)'}}>
+            <a className="item-link" href="https://github.com/carmelitadeleon/GEOG458_Final" rel="noopener noreferrer" target="_blank" style={{backgroundColor:'rgb(139, 139, 137)'}}>
               <div id="usexports" className="proj-image">
                 <div className="hover-overlay">
                   <h2>U.S. Export Analysis</h2>
@@ -51,17 +51,17 @@ class Home extends Component {
           </div>
         </div>
         <div className="social-area">
-          <a href="https://github.com/jinc132" target="_blank">
+          <a href="https://github.com/jinc132" rel="noopener noreferrer" target="_blank">
             <div className="circle">
               <i className="fa fa-github" id="social-media"></i>
             </div>
           </a>
-          <a href="https://www.linkedin.com/in/jinschang" target="_blank">
+          <a href="https://www.linkedin.com/in/jinschang" rel="noopener noreferrer" target="_blank">
             <div className="circle">
               <i className="fa fa-linkedin" id="social-media"></i>
             </div>
           </a>
-          <a href="mailto:Jinc132@gmail.com" target="_blank">
+          <a href="mailto:Jinc132@gmail.com" rel="noopener noreferrer" target="_blank">
             <div className="circle">
               <i className="fa fa-envelope" id="social-media"></i>
             </div>
