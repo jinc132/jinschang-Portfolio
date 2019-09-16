b916d878752d83a7c9c181f152a6af4224df0cb1
jinc132 <jinc132@u.washington.edu>
2019-09-15 17:44:21 -0700

    add word cloud

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'; 
-import { Link } from 'react-router-dom';
+import WordCloud from 'react-d3-cloud';
 import '../css/about.css';
 import '../css/home.css';
 import picture from '../css/img/jinPicture.png';
+
+const skillset = [
+    { text: 'Java', value: 50000 },
+    { text: 'HTML', value: 200 },
+    { text: 'CSS', value: 50 },
+    { text: 'JavaScript', value: 3400 },
+    { text: 'ReactJS', value: 2300 },
+    { text: 'NodeJS', value: 3400 },
+    { text: 'R', value: 300 },
+    { text: 'Python', value: 500 },
+    { text: 'VueJS', value: 520 },
+    { text: 'JUnit', value: 5042 },
+    { text: 'TypeScript', value: 5340 },
+    { text: 'Firebase', value: 5568 },
+    { text: 'SQL', value: 3290 },
+    { text: 'Tableau', value: 6320 },
+    { text: 'Figma', value: 2040 },
+    { text: 'Jupyter Notebook', value: 3920 },
+    { text: 'QGIS', value: 482 },
+    { text: 'ArcGIS', value: 554 },
+    { text: 'Communication', value: 490 },
+    { text: 'Trello', value: 1493 },
+    { text: 'Project Management', value: 903 }
+];
+
+const fontSizeMapper = word => Math.log2(word.value) * 5;
+const rotate = word => word.value % 60;
+
 class About extends Component {
     render() {
         return (
@@ -17,8 +45,22 @@ class About extends Component {
                     <div className="about-info">
                         <h2>Who is Jin?</h2>
                         <p>
-                            I am recent graduate from <a href="https://artsci.washington.edu/" rel="noopener noreferrer" target="_blank">University of Washington College of Arts and Sciences</a>.
-                            I received my bachelor's degree in <a href="https://geography.washington.edu/" rel="noopener noreferrer" target="_blank">Geographic Information Systems</a> and minored in <a href="https://ischool.uw.edu/programs/informatics" rel="noopener noreferrer" target="_blank">Informatics</a>.
+                            I am a recent graduate from <a href="https://artsci.washington.edu/" rel="noopener noreferrer" target="_blank">University of Washington</a> with
+                            a degree in <a href="https://geography.washington.edu/" rel="noopener noreferrer" target="_blank">Geographic Information Systems</a> and a
+                            minor in <a href="https://ischool.uw.edu/programs/informatics" rel="noopener noreferrer" target="_blank">Informatics</a>.<br/><br/>
+                            Originally, I started my academic career in the pharmaceutical field but, I was intrigued by the logic and collaborative environment that the 
+                            computer science field promotes. I have always had a passion for collaborating, sharing ideas and learning new things. These were some of the
+                            factors that motivated me to switch gears and pursue this field.<br /><br /> 
+                            
+                            Over the last few years, I’ve focused on building software projects that are both creative and help the community in new ways.
+                            After college, I worked on developing an interactive web map application for Statewide Poverty Action Network, a nonprofit organization that advocates for
+                            public policy.<br /><br /> 
+
+                            During my downtime, I have would go hiking, read on my sofa, or a collaborate on fun side projects. I’m experienced in languages like: 
+                        </p>
+                        <WordCloud data={skillset} fontSizeMapper={fontSizeMapper} rotate={rotate}/>
+                        <p>
+                            I would like to apply my skills in a team based setting to create software products that make a big impact.
                         </p>
                     </div>
                 </div>
